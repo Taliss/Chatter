79e9d01ca8b408cd3f34c79f925df0c8d80157bb
Taliss <iliyan.vlahov@gmail.com>
2017-10-04 21:01:27 +0300

    Add messages to reducers

--- ui/src/store.js
+++ ui/src/store.js
@@ -3,11 +3,10 @@ import { routerReducer } from 'react-router-redux';
 import { rooms, messages } from './reducers';
 import { router } from './middlewares';
 
-const reducer = combineReducers({ rooms, router: routerReducer });
+const reducer = combineReducers({ rooms, messages, router: routerReducer });
 
 export default createStore(
   reducer,
-  messages,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), // eslint-disable-line no-underscore-dangle
   applyMiddleware(router),
 );
